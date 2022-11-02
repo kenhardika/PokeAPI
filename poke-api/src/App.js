7f71095be5c0791e8307f92abb7982f9ac8da1ac
kenhardika <kenzamahardika@gmail.com>
2022-11-02 23:15:22 +0700

    Update all based on comment

--- poke-api/src/App.js
+++ poke-api/src/App.js
@@ -1,70 +1,49 @@
-import { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import TableNavigation from './TableNavigation';
 import Tablepokedex from './Tablepokedex';
-import fetchPokemon from './utils/fetchPokemon';
-import fetchDataNavigatePokemon from './utils/fetchDataNavigatePokemon';
 import fetchLinkPokemon from './utils/fetchLinkPokemon';
 
 
 function App() {
-  const [pokedex, setPokedex] = useState([]);
-  const [navigateTable, setNavigateTable] = useState([]);
-  const [offsetPage, setOffsetPage] = useState(0);
-  const INITIAL_FETCH = 20;
-  const fetchData = useCallback( async ()=>{
-    let arrayData = [];
+  const [pokedex, setPokedex] = useState({});
+  const fetchData = useCallback( async (offsetPage)=>{
             try{
-              const responseNavigate = await fetchDataNavigatePokemon(offsetPage); // for navigate purpose
-              const responseBase = await fetchPokemon(offsetPage + INITIAL_FETCH); //base exp is base data for our table, id, name, base exp.
-              setNavigateTable(responseNavigate.data);              
-              responseBase.map((item)=>{
-                return arrayData.push(item.data);
-              });
-                for(const data of arrayData){
-                  const location = await fetchLinkPokemon(data.location_area_encounters);
-                  data.location_area_encounters = location;
-                  for(const ability of data.abilities){
-                    const abils = await fetchLinkPokemon(ability.ability.url);
-                    ability.ability.url = abils;
-                  }
-                }
-              setPokedex(arrayData);
+              const response = await fetchLinkPokemon(offsetPage); 
+              setPokedex(response.data);              
               return;
             }
             catch{
               throw Error('Error fetch');
             }
-  }, [offsetPage, INITIAL_FETCH])
+  }, [])
 
   const handleNextPage = (e) =>{
     e.preventDefault();
-    if(navigateTable.next !== null){
-      setOffsetPage((cur)=>cur+20);
-      console.log('next 20');
+    if(pokedex.next !== null){
+      fetchData(pokedex.next);
       return
     }
     else return
   }
   const handlePrevPage = (e) =>{
     e.preventDefault();
-    if(navigateTable.previous !== null){
-      setOffsetPage((cur)=>cur-20);
-      console.log('prev 20');
+    if(pokedex.previous !== null){
+      fetchData(pokedex.previous);
       return
     }
     else return
   }
 
   useEffect(()=>{
-   fetchData(); 
+   fetchData("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0"); 
   }, [fetchData])
 
   return (
     <div className="App">
       {
         Object.keys(pokedex).length?
-        <Tablepokedex data = {pokedex}/> :
+        <Tablepokedex data = {pokedex.results}/> :
         <p>Loading</p> 
       }
       <TableNavigation handlePrevPage={handlePrevPage} handleNextPage={handleNextPage} />
--- poke-api/src/Tablepokedex.js
+++ poke-api/src/Tablepokedex.js
@@ -1,22 +1,80 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/esm/Table';
 import uniqid from "uniqid";
-import AbilityModal from './components/AbilityModal';
-import StatusModal from './components/StatusModal';
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import fetchLinkPokemon from './utils/fetchLinkPokemon';
 
 function Tablepokedex({data}) {
-  // console.log(data[0].location_area_encounters.data[0].location_area.name);
-  // console.log(data);
+  const [showAbility, setShowAbility] = useState(false);
+  const [showStatus, setShowStatus] = useState(false);
+
+  const [ability, setAbility] = useState([]);
+  const [status, setStatus] = useState([]);
+  
+  const handleClose = () =>  {
+    setShowAbility(false);
+    setShowStatus(false);
+  };
+  const handleShow = (param) => {
+    if(param === 'ability'){
+      setShowStatus(false);
+      setShowAbility(true);
+    }
+    else{
+      setShowAbility(false);
+      setShowStatus(true);
+    }
+  };
+
+  const handleClickCallAPI = async (name, type)=>{
+      const responseAPI = await fetchLinkPokemon(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      if(type === 'abilities'){
+        setAbility(responseAPI.data[type]);
+      }
+      else if(type === 'stats'){
+        setStatus(responseAPI.data[type])
+      }
+      return ;
+  }
+
     return (
         <div className='tablePokedex'>
+          <Modal show={showAbility} onHide={handleClose} dialogClassName='custom-dialog' >
+            <Modal.Header closeButton>
+              <Modal.Title>Ability:</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {
+                ability.map((item)=><h5 key={uniqid()}> {item.ability.name} </h5>)
+              }
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        <Modal show={showStatus} onHide={handleClose} dialogClassName='custom-dialog'>
+            <Modal.Header closeButton>
+              <Modal.Title>Status:</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {
+                status.map((item)=><h5 key={uniqid()}> {item.stat.name}: {item.base_stat} </h5>)
+              }
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>id</th>
               <th>Nama Pokemon</th>
-              <th>Base Experience</th>
-              <th>Location_area_encounters</th>
               <th>Actions</th>
             </tr>
           </thead>
@@ -24,12 +82,18 @@ function Tablepokedex({data}) {
             {
               data.map((res)=>
               <tr key={uniqid()}>
-                <td key={uniqid()}>{res.id}</td>
                 <td key={uniqid()}>{res.name}</td>
-                <td key={uniqid()}>{res.base_experience}</td>
-                <td width={'60%'} key={uniqid()}> {res.location_area_encounters.data.map((item)=> item.location_area.name + ', ')} </td>
                 <td className='layerButton'>
-                    <AbilityModal data={res.abilities}/> <StatusModal data={res.stats}/>
+                    <Button variant="info" 
+                        onClick={()=>{
+                            handleShow('ability');
+                            handleClickCallAPI(res.name, 'abilities');
+                    }}>Ability</Button>
+                    <Button variant="info" 
+                        onClick={()=>{
+                            handleShow('status');
+                            handleClickCallAPI(res.name, 'stats');
+                    }}>Status</Button>
                 </td>
               </tr>)
             }
